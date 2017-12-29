2b3008f8f51388d0fb10ff45d273a34fdf013902
tdreid <trevor.d.reid@gmail.com>
2017-12-29 23:39:56 +0000

    Drop it to the flo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'GQeltt | A game where you indicate which number is larger to score points' });
+  res.render('index', {
+    title:
+      'GQeltt | A game where you indicate which number is larger to score points'
+  });
 });
 
 module.exports = router;
